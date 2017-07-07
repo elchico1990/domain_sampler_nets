8b405960bb40fcff443b6ebba4512f89eba434b9
Pietro Morerio <pietro.morerio@iit.it>
2017-07-07 11:53:53 +0200

    removing models from repo

--- dtn/download.sh
+++ dtn/download.sh
@@ -1,6 +1,7 @@
 mkdir -p mnist
 mkdir -p svhn
 mkdir -p usps
+mkdir -p office
 
 wget -O svhn/train_32x32.mat http://ufldl.stanford.edu/housenumbers/train_32x32.mat
 wget -O svhn/test_32x32.mat http://ufldl.stanford.edu/housenumbers/test_32x32.mat
