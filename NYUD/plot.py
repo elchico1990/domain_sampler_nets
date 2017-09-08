d22bccdbaa4004c42bbfefe565988c8c95f9b7ca
Pietro Morerio <pietro.morerio@iit.it>
2017-09-08 08:57:27 +0200

    [NYUD] plot dsn

--- NYUD/plot.py
+++ NYUD/plot.py
@@ -17,5 +17,19 @@ for i, line in enumerate(ll):
 		g = line.split('E: [')[1]
 		G.append(g[:6])
 
-plt.plot()
+plt.subplot(3,1,1)
+plt.plot(D)
+plt.ylabel('Discriminator Loss')
+plt.xlabel('Iterations (x10)')
+
+plt.subplot(3,1,2)
+plt.plot(G)
+plt.ylabel('Generator Loss')
+plt.xlabel('Iterations (x10)')
+
+plt.subplot(3,1,3)
+plt.plot(accuracies)
+plt.ylabel('Test Accuracy')
+plt.xlabel('Iterations (x20)')
+
 plt.show()
