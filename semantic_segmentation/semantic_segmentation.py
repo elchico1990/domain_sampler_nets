a56ea7aaf6cc5cd59ba6fcd64abfe7da9137e044
ricvolpi <riccardo.volpi@aol.com>
2017-10-10 20:56:42 +0200

    update

--- semantic_segmentation/semantic_segmentation.py
+++ semantic_segmentation/semantic_segmentation.py
@@ -48,51 +48,41 @@ def bilinear_upsample_weights(factor, number_of_classes):
 # %matplotlib inline
 #
 # from __future__ import division
-#
-# import os
-# import sys
-# import tensorflow as tf
-# import skimage.io as io
-# import numpy as np
-#
-# os.environ["CUDA_VISIBLE_DEVICES"] = '1'
-# sys.path.append("/home/dpakhom1/workspace/my_models/slim/")
-# checkpoints_dir = '/home/dpakhom1/checkpoints'
-#
-# image_filename = 'cat.jpg'
-# annotation_filename = 'cat_annotation.png'
-#
-# image_filename_placeholder = tf.placeholder(tf.string)
-# annotation_filename_placeholder = tf.placeholder(tf.string)
-# is_training_placeholder = tf.placeholder(tf.bool)
-#
-# feed_dict_to_use = {image_filename_placeholder: image_filename,
-#                     annotation_filename_placeholder: annotation_filename,
-#                     is_training_placeholder: True}
-#
-# image_tensor = tf.read_file(image_filename_placeholder)
-# annotation_tensor = tf.read_file(annotation_filename_placeholder)
-#
-# image_tensor = tf.image.decode_jpeg(image_tensor, channels=3)
-# annotation_tensor = tf.image.decode_png(annotation_tensor, channels=1)
-#
-# # Get ones for each class instead of a number -- we need that
-# # for cross-entropy loss later on. Sometimes the groundtruth
-# # masks have values other than 1 and 0.
-# class_labels_tensor = tf.equal(annotation_tensor, 1)
-# background_labels_tensor = tf.not_equal(annotation_tensor, 1)
-#
-# # Convert the boolean values into floats -- so that
-# # computations in cross-entropy loss is correct
-# bit_mask_class = tf.to_float(class_labels_tensor)
-# bit_mask_background = tf.to_float(background_labels_tensor)
-#
-# combined_mask = tf.concat(concat_dim=2, values=[bit_mask_class,
-#                                                 bit_mask_background])
-#
-# # Lets reshape our input so that it becomes suitable for
-# # tf.softmax_cross_entropy_with_logits with [batch_size, num_classes]
-# flat_labels = tf.reshape(tensor=combined_mask, shape=(-1, 2))
+
+import os
+import sys
+import tensorflow as tf
+import skimage.io as io
+import numpy as np
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+sys.path.append("/home/dpakhom1/workspace/my_models/slim/")
+checkpoints_dir = '/home/dpakhom1/checkpoints'
+image_filename = 'cat.jpg'
+annotation_filename = 'cat_annotation.png'
+image_filename_placeholder = tf.placeholder(tf.string)
+annotation_filename_placeholder = tf.placeholder(tf.string)
+is_training_placeholder = tf.placeholder(tf.bool)
+feed_dict_to_use = {image_filename_placeholder: image_filename,
+                    annotation_filename_placeholder: annotation_filename,
+                    is_training_placeholder: True}
+image_tensor = tf.read_file(image_filename_placeholder)
+annotation_tensor = tf.read_file(annotation_filename_placeholder)
+image_tensor = tf.image.decode_jpeg(image_tensor, channels=3)
+annotation_tensor = tf.image.decode_png(annotation_tensor, channels=1)
+# Get ones for each class instead of a number -- we need that
+# for cross-entropy loss later on. Sometimes the groundtruth
+# masks have values other than 1 and 0.
+class_labels_tensor = tf.equal(annotation_tensor, 1)
+background_labels_tensor = tf.not_equal(annotation_tensor, 1)
+# Convert the boolean values into floats -- so that
+# computations in cross-entropy loss is correct
+bit_mask_class = tf.to_float(class_labels_tensor)
+bit_mask_background = tf.to_float(background_labels_tensor)
+combined_mask = tf.concat(concat_dim=2, values=[bit_mask_class,
+                                                bit_mask_background])
+# Lets reshape our input so that it becomes suitable for
+# tf.softmax_cross_entropy_with_logits with [batch_size, num_classes]
+flat_labels = tf.reshape(tensor=combined_mask, shape=(-1, 2))
 
 
 
@@ -123,9 +113,9 @@ from preprocessing.vgg_preprocessing import (_mean_image_subtraction,
 
 upsample_factor = 32
 number_of_classes = 2
-log_folder = '/home/dpakhom1/tf_projects/segmentation/log_folder'
+log_folder = './logs'
 
-vgg_checkpoint_path = os.path.join(checkpoints_dir, 'vgg_16.ckpt')
+vgg_checkpoint_path = './vgg_16.ckpt'
 
 # Convert image to float32 before subtracting the
 # mean pixel value
