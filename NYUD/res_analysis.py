51b04a515a4f3075a4419b9bb219b65f293a72ce
Pietro Morerio <pietro.morerio@iit.it>
2017-11-13 16:30:49 +0100

    [NYUD] plot results wrong colors

--- NYUD/res_analysis.py
+++ NYUD/res_analysis.py
@@ -2,15 +2,17 @@ import cPickle
 import matplotlib.pyplot as plt
 
 data_dir = './'
-with open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb') as f:
-    qwe = cPickle.load(f)
-    plt.plot(qwe)
-
-data_dir = './'
 with open(data_dir + 'train_dsn_test_accuracies.pkl','rb') as f:
     qwe = cPickle.load(f)
-    plt.plot(qwe)
+plt.plot(qwe)
 
+data_dir = './'
+with open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb') as f:
+    qwe = cPickle.load(f)
+plt.plot(qwe)
+
+plt.ylabel('Accuracy %')
+plt.xlabel('Iterations (x20)')
 
 plt.show()
 
