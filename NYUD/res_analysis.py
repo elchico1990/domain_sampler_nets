d77cb530d0b011cb80528e0b471868e27ab5c120
Pietro Morerio <pietro.morerio@iit.it>
2017-11-13 14:02:22 +0100

    [NYUD] plot results

---
+++ NYUD/res_analysis.py
@@ -0,0 +1,16 @@
+import cPickle
+import matplotlib.pyplot as plt
+
+data_dir = './'
+with open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb') as f:
+    qwe = cPickle.load(f)
+    plt.plot(qwe)
+
+data_dir = './'
+with open(data_dir + 'train_dsn_test_accuracies.pkl','rb') as f:
+    qwe = cPickle.load(f)
+    plt.plot(qwe)
+
+
+plt.show()
+
