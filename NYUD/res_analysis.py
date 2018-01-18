b15b61c0efe9de3dde80ee4810b8530970e8c382
Pietro Morerio <pietro.morerio@gmail.com>
2018-01-18 17:56:02 +0100

    asd

--- NYUD/res_analysis.py
+++ NYUD/res_analysis.py
@@ -2,19 +2,21 @@ import cPickle
 import matplotlib.pyplot as plt
 
 data_dir = './'
-with open(data_dir + 'train_dsn_test_accuracies_0.pkl','rb') as f:
+with open(data_dir + 'train_dsn_test_accuracies.pkl','rb') as f:
     qwe = cPickle.load(f)
 plt.plot(qwe)
+
+#~ data_dir = './'
+#~ with open(data_dir + 'train_dsn_test_accuracies_0.pkl','rb') as f:
+    #~ qwe = cPickle.load(f)
+#~ plt.plot(qwe)
 
 data_dir = './'
 with open(data_dir + 'train_adda_shared_test_accuracies_0.pkl','rb') as f:
     qwe = cPickle.load(f)
 plt.plot(qwe)
 
-data_dir = './'
-with open(data_dir + 'train_dsn_test_accuracies.pkl','rb') as f:
-    qwe = cPickle.load(f)
-plt.plot(qwe)
+
 
 plt.ylabel('Accuracy %')
 plt.xlabel('Iterations (x20)')
