81d7cef4227f616826716e025878d03135608657
Pietro Morerio <pietro.morerio@iit.it>
2018-01-23 16:29:20 +0100

    res_analyzer exceptions

--- Office_resnet/res_analysis.py
+++ Office_resnet/res_analysis.py
@@ -2,24 +2,27 @@ import cPickle
 import matplotlib.pyplot as plt
 
 data_dir = './'
-with open(data_dir + 'train_adda_test_accuracies.pkl','rb') as f:
+try:
+    f = open(data_dir + 'train_adda_test_accuracies.pkl','rb')
     qwe = cPickle.load(f)
-plt.plot(qwe)
+    plt.plot(qwe)
+except:
+    print 'train_adda_test_accuracies.pkl not found '
 
-data_dir = './'
-with open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb') as f:
+try:
+    f = open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb')
     qwe = cPickle.load(f)
-plt.plot(qwe)
+    plt.plot(qwe)
+except:
+    print 'train_adda_shared_test_accuracies.pkl not found '
 
-#~ data_dir = './'
-#~ with open(data_dir + 'train_dsn_test_accuracies_0.pkl','rb') as f:
-    #~ qwe = cPickle.load(f)
-#~ plt.plot(qwe)
+try:
+    f = open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb')
+    qwe = cPickle.load(f)
+    plt.plot(qwe)
+except:
+    print 'train_dsn_test_accuracies.pkl not found '
 
-#~ data_dir = './'
-#~ with open(data_dir + 'NYUD_test_accuracies.pkl','rb') as f:
-    #~ qwe = cPickle.load(f)
-#~ plt.plot(qwe)
 
 plt.ylabel('Accuracy %')
 plt.xlabel('Iterations (x50)')
