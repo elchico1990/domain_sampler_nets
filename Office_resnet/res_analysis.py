467b1c9ed2e440c464ac4b7ee974feb1fc8bd30f
Pietro Morerio <pietro.morerio@gmail.com>
2018-01-23 16:30:57 +0100

    res_analyzer dsn

--- Office_resnet/res_analysis.py
+++ Office_resnet/res_analysis.py
@@ -17,7 +17,7 @@ except:
     print 'train_adda_shared_test_accuracies.pkl not found '
 
 try:
-    f = open(data_dir + 'train_adda_shared_test_accuracies.pkl','rb')
+    f = open(data_dir + 'train_dsn_test_accuracies.pkl','rb')
     qwe = cPickle.load(f)
     plt.plot(qwe)
 except:
